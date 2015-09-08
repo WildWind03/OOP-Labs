343ee27a851e9ff313ff9150e7615a62b569bbda
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2015-09-08 05:51:32 +0000

    Finished version

--- EVM_laboratory_1/main.cpp
+++ EVM_laboratory_1/main.cpp
@@ -1,29 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	FILE *file = NULL;
+	FILE *input_file = NULL;
 	unsigned int count_of_numbers = 0;
-	file = fopen("in.txt", "r");
-	if (file == NULL)
+	input_file = fopen("in.txt", "r");
+	if (input_file == NULL)
 	{
-		cout << "in.txt was not found";
+        printf ("in.txt was not found");
 		return -1;
 	}
-	cin >> count_of_numbers;
-	int array[count_of_numbers];
-	for (int i=0; i<count_of_numbers; i++)
+	if (fscanf (input_file, "%ud", &count_of_numbers) <= 0)
+    {
+        printf ("Incorrect input");
+    }
+	int *numbers_for_sort = (int*) malloc(sizeof(int) * count_of_numbers);
+	for (int i = 0; i < count_of_numbers; i++)
 	{
-		cin >> array[i];
+        fscanf (input_file, "%d", &numbers_for_sort[i]);
 	}
-	fclose(f);
+	int temp;
+	for (int i = 0; i < count_of_numbers; i++)
+    {
+        for (int k = 0; k < count_of_numbers - i; k++)
+        {
+            if (k + 1 < count_of_numbers)
+            {
+                if (numbers_for_sort[k] > numbers_for_sort[k + 1])
+                {
+                    temp = numbers_for_sort[k + 1];
+                    numbers_for_sort[k + 1] = numbers_for_sort[k];
+                    numbers_for_sort[k] = temp;
+                }
+            }
+        }
+    }
+    FILE *output_file;
+    output_file = fopen ("out.txt", "w");
+    if (output_file == NULL)
+    {
+        printf  ("Can't create output file");
+        return -1;
+    }
+    for (int i=0; i<count_of_numbers; i++)
+    {
+        fprintf (output_file, "%d ", numbers_for_sort[i]);
+    }
+    printf ("The array has been sorted!");
+    fclose(output_file);
+	fclose(input_file);
 	return 0;
-}
-
-bool isSorted(int *array, int count_of_numbers)
-{
-	for (int i = 0; i < count_of_numbers; i++) 
-	{
-		
-	}
 }
