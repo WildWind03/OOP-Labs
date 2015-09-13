b515283036f92e022a994ddf6b7e52f2be5dcdaa
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2015-09-13 06:55:02 +0000

    Release version

--- trunk/EVM_laboratory_1/main.cpp
+++ trunk/EVM_laboratory_1/main.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main()
 {
 	FILE *input_file = NULL;
 	unsigned int count_of_numbers = 0;
-	input_file = fopen("in.txt", "r");
+	input_file = fopen("array.txt", "r");
 	if (input_file == NULL)
 	{
         printf ("in.txt was not found");
@@ -14,13 +15,20 @@ int main()
 	if (fscanf (input_file, "%ud", &count_of_numbers) <= 0)
     {
         printf ("Incorrect input");
+        return -1;
     }
 	int *numbers_for_sort = (int*) malloc(sizeof(int) * count_of_numbers);
 	for (int i = 0; i < count_of_numbers; i++)
 	{
-        fscanf (input_file, "%d", &numbers_for_sort[i]);
+        if (fscanf (input_file, "%d", &numbers_for_sort[i]) <=0)
+        {
+            printf ("Incorrect input");
+            return -1;
+        }
 	}
-	int temp;
+	int temp = 0;
+	long start_time, end_time;
+	start_time = GetTickCount();
 	for (int i = 0; i < count_of_numbers; i++)
     {
         for (int k = 0; k < count_of_numbers - i; k++)
@@ -36,6 +44,7 @@ int main()
             }
         }
     }
+    end_time = GetTickCount();
     FILE *output_file;
     output_file = fopen ("out.txt", "w");
     if (output_file == NULL)
@@ -47,7 +56,10 @@ int main()
     {
         fprintf (output_file, "%d ", numbers_for_sort[i]);
     }
-    printf ("The array has been sorted!");
+    printf ("The array has been sorted!\n");
+    long total_time = end_time - start_time;
+    printf ("The program has been working for %ld milliseconds (or %ld.%ld seconds)\n", total_time, total_time / 1000, total_time - ((long)(total_time / 1000)) * 1000);
+    system ("PAUSE");
     fclose(output_file);
 	fclose(input_file);
 	return 0;
