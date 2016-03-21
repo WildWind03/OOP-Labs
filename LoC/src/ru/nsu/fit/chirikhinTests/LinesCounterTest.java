9144da376bbf2b481a44fb15a0815138a2a50872
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2016-03-21 05:21:00 +0000

    Fixed bug with one filter

--- LoC/src/ru/nsu/fit/chirikhinTests/LinesCounterTest.java
+++ LoC/src/ru/nsu/fit/chirikhinTests/LinesCounterTest.java
@@ -3,10 +3,7 @@ package ru.nsu.fit.chirikhinTests;
 import org.junit.Test;
 import ru.nsu.fit.chirikhin.LinesCounter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
 import static org.junit.Assert.assertTrue;
 
@@ -16,7 +13,7 @@ public class LinesCounterTest {
     }
 
     @Test
-    public void countLinesTest() throws FileNotFoundException, UnsupportedEncodingException {
+    public void countLinesTest() throws IOException {
         try (PrintWriter writer = new PrintWriter("test.txt", "UTF-8")) {
             writer.println("The first line");
             writer.println("The second line");
