71bf5162fc61fd2766932d9664c623464cfdad51
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2016-04-24 04:11:23 +0000

    (+) GUI is in developing

--- Factory/src/ru/nsu/ccfit/chirikhin/factory/FactoryController.java
+++ Factory/src/ru/nsu/ccfit/chirikhin/factory/FactoryController.java
@@ -10,13 +10,17 @@ import java.util.Observable;
 
 public class FactoryController {
     private Logger logger = Logger.getLogger(FactoryController.class.getName());
-    private final String pathToFile;
     private final Factory factory;
+    private FXMLViewController fxmlViewController;
+    private ConfigParser configParser;
 
     public FactoryController(String pathToFile, FXMLViewController fxmlViewController) throws InvalidConfigException, IOException, DeveloperBugException{
+        this.fxmlViewController = fxmlViewController;
+        configParser = new ConfigParser(pathToFile);
         logger.info("------------------------ Factory Controller has started!");
-        this.pathToFile = pathToFile;
-        factory = new Factory(pathToFile);
+        factory = new Factory(configParser.getWorkersCount(), configParser.getDealersCount(), configParser.getAccessorySupplCount(), configParser.getCarStorageSize(), configParser.getEngineStorageSize(),
+                configParser.getAccessoryStorageSize(), configParser.getCarBodyStorageSize(), configParser.isLog());
+
         factory.setOnEngineStorageChangedHandler(new Handler(fxmlViewController) {
             @Override
             public void update(Observable o, Object arg) {
@@ -48,6 +52,15 @@ public class FactoryController {
 
     public void startFactory() throws DeveloperBugException, InvalidConfigException, InterruptedException, IOException {
         logger.info("Factory has started!");
+        initFXMLView();
         factory.start();
+    }
+
+    public void initFXMLView() {
+        Platform.runLater(() -> {
+            fxmlViewController.setCountOfAccessoryProducers(configParser.getAccessorySupplCount());
+            fxmlViewController.setCountOfDealers(configParser.getDealersCount());
+            fxmlViewController.setCountOfWorkers(configParser.getWorkersCount());
+        });
     }
 }
