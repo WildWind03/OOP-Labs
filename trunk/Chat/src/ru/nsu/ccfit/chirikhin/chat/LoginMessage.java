a021699f987f2989918b6e371cf52039f339c529
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2016-05-15 09:01:56 +0000

    (+) Usernames

--- trunk/Chat/src/ru/nsu/ccfit/chirikhin/chat/LoginMessage.java
+++ trunk/Chat/src/ru/nsu/ccfit/chirikhin/chat/LoginMessage.java
@@ -6,8 +6,25 @@ import ru.nsu.ccfit.chirikhin.chat.server.ServerMessageController;
 public class LoginMessage extends Message{
     private static final Logger logger = Logger.getLogger(LoginMessage.class.getName());
 
+    private final String username;
+    private final String chatClientName;
+
+    public LoginMessage(String username, String chatClientName) {
+        this.username = username;
+        this.chatClientName = chatClientName;
+    }
+
     @Override
     public void process(ServerMessageController serverMessageController) {
+
+    }
+
+    public String getChatClientName() {
+        return chatClientName;
+    }
+
+    public String getUsername() {
+        return username;
 
     }
 }
