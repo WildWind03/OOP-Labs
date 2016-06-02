e4a594b62ea49e6b779fa161277c365e4e81fd32
14203_chirikhin <14203_chirikhin@4da7a52d-4af8-48ca-83ac-73813fdcc46a>
2016-06-02 15:36:25 +0000

    (+) check for null in server package (not fully)

--- trunk/Chat/src/ru/nsu/ccfit/chirikhin/chat/server/NicknameBusyException.java
+++ trunk/Chat/src/ru/nsu/ccfit/chirikhin/chat/server/NicknameBusyException.java
@@ -5,23 +5,7 @@ import org.apache.log4j.Logger;
 public class NicknameBusyException extends Exception {
     private static final Logger logger = Logger.getLogger(NicknameBusyException.class.getName());
 
-    public NicknameBusyException() {
-        super();
-    }
-
     public NicknameBusyException(String message) {
         super(message);
-    }
-
-    public NicknameBusyException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NicknameBusyException(Throwable cause) {
-        super(cause);
-    }
-
-    protected NicknameBusyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
